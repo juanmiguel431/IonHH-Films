d6830da62675062e40a6064f0fec46653b6d36c0
juanmiguel431 <juanmiguel431@gmail.com>
2023-03-03 21:22:09 -0400

    Entity Framework setup and ApplicationDbContext added.

---
+++ Films/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Films.EntitiesConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Films.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new MovieConfiguration());
+    }
+}
