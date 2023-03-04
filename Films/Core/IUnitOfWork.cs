49d541c036f0668b4534dda48c842c277840f8e7
juanmiguel431 <juanmiguel431@gmail.com>
2023-03-03 22:56:13 -0400

    UnitOfWork added.

---
+++ Films/Core/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Films.Core.Repositories;
+
+namespace Films.Core;
+
+public interface IUnitOfWork : IDisposable
+{
+    IMovieRepository Movies { get; }
+    int SaveChanges();
+}
---
+++ Films/Persistence/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Films.Core;
+using Films.Core.Repositories;
+using Films.Persistence.Repositories;
+
+namespace Films.Persistence;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    private IMovieRepository? _movies;
+
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IMovieRepository Movies => _movies ??= new MovieRepository(_context);
+
+    public int SaveChanges()
+    {
+        return _context.SaveChanges();
+    }
+    
+    public void Dispose()
+    {
+        _context.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
